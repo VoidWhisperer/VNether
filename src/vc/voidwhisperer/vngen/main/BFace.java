396de2f930ff3d0566e6ae2347acea1c46e1bcaf
VoidWhisperer <voidwhisperer@voidchar.com>
2012-08-26 11:03:47 -0300

    Remove some text from BFace.java
    Accidently left copyright in.

--- src/vc/voidwhisperer/vngen/main/BFace.java
+++ src/vc/voidwhisperer/vngen/main/BFace.java
@@ -1,12 +1,3 @@
-/*******************************************************************************
-* Copyright (C) 2012 VoidWhisperer
-*
-* This work is licensed under the Creative Commons
-* Attribution-NonCommercial-NoDerivs 3.0 Unported License. To view a copy of
-* this license, visit http://creativecommons.org/licenses/by-nc-nd/3.0/ or send
-* a letter to Creative Commons, 171 Second Street, Suite 300, San Francisco,
-* California, 94105, USA.
-******************************************************************************/
 package vc.voidwhisperer.vngen.main;
 
 import org.bukkit.util.Vector;
